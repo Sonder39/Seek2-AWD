6a4f33530f153f242c23bf49eb2702b1a82f6088
Sonder39 <CyberAnts@example.com>
2024-06-05 09:17:07 +0800

    修改部分路由设计，完成管理大屏/admin/HomeView.vue组件设计

---
+++ frontend/src/components/admin/Progress.vue
@@ -0,0 +1,22 @@
+<script setup>
+import {ref} from "vue"
+
+const startDate = new Date('2024-05-30T21:51:00').getTime()
+const endDate = new Date('2024-05-30T21:52:00').getTime()
+const progressRef = ref(0)
+
+setInterval(() => {
+  const localDate = new Date().getTime()
+  progressRef.value = ((localDate - startDate) / (endDate - startDate)) * 100
+}, 1000)
+</script>
+
+<template>
+  <div class="progress">
+    <div class="progress-bar progress-bar-striped progress-bar-animated" :style="{width: progressRef + '%'}">
+    </div>
+  </div>
+</template>
+
+<style scoped>
+</style>
